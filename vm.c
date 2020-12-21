957ffd2df3672e0910911b8e5cd9e14d7f457b53
Meng Xiangzhuo <aumo@foxmail.com>
2020-12-21 21:11:31 +0800

    Add string objects.

--- vm.c
+++ vm.c
@@ -1,10 +1,13 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "common.h"
+#include "debug.h"
+#include "object.h"
+#include "memory.h"
 #include "vm.h"
 #include "compiler.h"
-#include "debug.h"
 
 VM vm;
 
@@ -29,11 +32,13 @@ static void runtimeError(const char *format, ...)
 
 void initVM()
 {
+    vm.objects = NULL;
     resetStack();
 }
 
 void freeVM()
 {
+    freeObjects();
 }
 
 void push(Value value)
@@ -53,6 +58,20 @@ static Value peek(int distance)
 
 static bool isFalsey(Value value) {
     return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
+static void concatenate() {
+    ObjString *b = AS_STRING(pop());
+    ObjString *a = AS_STRING(pop());
+
+    int length = a->length + b->length;
+    char *chars = ALLOCATE(char, length + 1);
+    memcpy(chars, a->chars, a->length);
+    memcpy(chars + a->length, b->chars, b->length);
+    chars[length] = '\0';
+
+    ObjString *result = takeString(chars, length);
+    push(OBJ_VAL(result));
 }
 
 static InterpretResult run()
@@ -97,6 +116,7 @@ static InterpretResult run()
         }
         case OP_NIL:
             push(NIL_VAL);
+            break;
         case OP_TRUE:
             push(BOOL_VAL(true));
             break;
@@ -116,7 +136,18 @@ static InterpretResult run()
             BINARY_OP(BOOL_VAL, <);
             break;
         case OP_ADD:
-            BINARY_OP(NUMBER_VAL, +);
+            if (IS_STRING(peek(0)) && IS_STRING(peek(1))) {
+                concatenate();
+            } else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1))) {
+                double b = AS_NUMBER(pop());
+                double a = AS_NUMBER(pop());
+                push(NUMBER_VAL(a + b));
+            } else {
+                runtimeError(
+                    "Operands must be two numbers or two stirngs."
+                );
+                return INTERPRET_RUNTIME_ERROR;
+            }
             break;
         case OP_SUBSTRACT:
             BINARY_OP(NUMBER_VAL, -);
